1e9499daa2ab4439c4e6176a1af1c3717ce1fee5
Vycheslav Tumanov <tumanov@umarsh.ru>
2022-07-25 18:36:51 +0300

    add server/client socket

--- app/src/main/java/ru/umarsh/serversockettest/MainActivity.kt
+++ app/src/main/java/ru/umarsh/serversockettest/MainActivity.kt
@@ -1,11 +1,124 @@
 package ru.umarsh.serversockettest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
+import java.io.OutputStream
+import java.io.PrintStream
+import java.net.NetworkInterface
+import java.net.ServerSocket
+import java.net.Socket
+import java.net.SocketException
+import java.util.concurrent.atomic.AtomicBoolean
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var info: TextView
+    lateinit var infoip: TextView
+    lateinit var msg: TextView
+    var serverSocket: ServerSocket? = null
+    var message = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        infoip.text = getIpAddress()
+        val socketServerThread = Thread(SocketServerThread())
+        socketServerThread.start()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (serverSocket != null) {
+            try {
+                serverSocket!!.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun initView() {
+        info = findViewById(R.id.info)
+        infoip = findViewById(R.id.infoip)
+        msg = findViewById(R.id.msg)
+
+    }
+
+    private fun getIpAddress(): String {
+        var ip = ""
+        try {
+            val enumNetworkInterfaces = NetworkInterface.getNetworkInterfaces()
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                val networkInterface = enumNetworkInterfaces.nextElement()
+                val enumInetAddress = networkInterface.inetAddresses
+                while (enumInetAddress.hasMoreElements()) {
+                    val inetAddress = enumInetAddress.nextElement()
+                    if (inetAddress.isSiteLocalAddress) {
+                        ip += ("SiteLocalAddress: " + inetAddress.hostAddress) + "\n"
+                    }
+                }
+            }
+        } catch (e: SocketException) {
+            e.printStackTrace()
+            ip += """Something Wrong! $e""".trimIndent()
+        }
+        return ip
+    }
+
+    inner class SocketServerThread : Thread() {
+
+        val SocketServerPORT = 8080
+        var count = 0
+
+        override fun run() {
+            try {
+                serverSocket = ServerSocket(SocketServerPORT)
+                runOnUiThread {
+                    info.text = "I'm waiting here: ${serverSocket?.localPort}"
+                }
+                while (true) {
+                    val socket = serverSocket!!.accept()
+                    count++;
+                    message += "#" + count + " from " + socket.inetAddress + ":" + socket.port + "\n"
+                    runOnUiThread {
+                        msg.text = message
+                    }
+                    val socketServerReplyThread = SocketServerReplyThread(socket, count)
+                    socketServerReplyThread.start()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    inner class SocketServerReplyThread(private val socket: Socket, private val c: Int) : Thread() {
+        val hostThreadSocket: Socket = socket
+        val cnt: Int = c
+
+        override fun run() {
+            val outputStream: OutputStream
+            val msgReply = "Hello from Android, you are # $cnt"
+            try {
+                outputStream = hostThreadSocket.getOutputStream()
+                val printStream = PrintStream(outputStream)
+                printStream.print(msgReply)
+                printStream.close()
+                message += "replayed: $msgReply \n"
+                runOnUiThread {
+                    msg.text = message
+                }
+            } catch (e: java.lang.Exception) {
+                message += "Something wrong! $e \n"
+                e.printStackTrace()
+            }
+            runOnUiThread {
+                msg.text = message
+            }
+        }
+    }
+
 }
--- clientsockettest/src/main/java/ru/umarsh/clientsockettest/MainActivity.kt
+++ clientsockettest/src/main/java/ru/umarsh/clientsockettest/MainActivity.kt
@@ -1,11 +1,93 @@
 package ru.umarsh.clientsockettest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.net.ServerSocket
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var textResponse: TextView
+    lateinit var editTextAddress: EditText
+    lateinit var editTextPort: TextView
+    lateinit var buttonConnect: Button
+    lateinit var buttonClear: Button
+    var response = ""
+    var socket: Socket? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
+        buttonClear.setOnClickListener {
+            textResponse.text = ""
+        }
+        buttonConnect.setOnClickListener {
+            connectInit()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (socket != null) {
+            try {
+                socket!!.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+    private fun connectInit() {
+        val address = editTextAddress.text.toString()
+        val port = editTextPort.text.toString().toInt()
+        val clientSocketThread = Thread(ClientSocket(address, port))
+        clientSocketThread.start()
+    }
+
+    private fun initView() {
+        editTextAddress = findViewById(R.id.address)
+        editTextPort = findViewById(R.id.port)
+        buttonConnect = findViewById(R.id.connect)
+        buttonClear = findViewById(R.id.clear)
+        textResponse = findViewById(R.id.response)
+    }
+
+    inner class ClientSocket(private val address: String, private val port: Int): Thread() {
+
+        val addressIp = address
+        val portIp = port
+
+        override fun run() {
+            try {
+                socket = Socket(address, port)
+                val byteArrayOutputStream = ByteArrayOutputStream(1024)
+                val buffer = ByteArray(1024)
+                val bytesRead: Int
+                val inputStream = socket!!.getInputStream()
+                bytesRead = inputStream.read(buffer)
+                while (bytesRead != -1) {
+                    byteArrayOutputStream.write(buffer, 0, bytesRead)
+                    response += byteArrayOutputStream.toString("UTF-8")
+                    runOnUiThread {
+                        textResponse.text = response
+                    }
+                }
+                Log.d(this.javaClass.name, ">>>> Response: $response")
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+
+        }
     }
 }
